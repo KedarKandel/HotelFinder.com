a4aa91f196115a5a2651a8b777718127ed575925
Kedar Kandel <kandelkedar07@gmail.com>
2022-05-20 19:28:30 +0300

    add featurePrice component

---
+++ client/src/components/featuredPrice/FeaturedPrice.jsx
@@ -0,0 +1,80 @@
+import "./featuredPrice.css";
+
+const FeaturedPrice = () => {
+  return (
+    <div className="fpContainer">
+      <div className="fpItem">
+        <img
+          src="https://media.istockphoto.com/photos/3d-rendering-beautiful-luxury-bedroom-suite-in-hotel-with-tv-picture-id1066999762?k=20&m=1066999762&s=612x612&w=0&h=BitPXyhBFZQHMfpC9ikX_DReVK6Rd28hH9pRoZW8YAs="
+          alt=""
+          className="fpImg"
+        />
+        <h4>Standard</h4>
+        <h6>Helsinki</h6>
+        <h4>Starting from € 150 </h4>
+        <div className="rating">
+          <button>9.8</button>
+          <h6>Exceptional</h6>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://media.istockphoto.com/photos/3d-rendering-beautiful-luxury-bedroom-suite-in-hotel-with-tv-picture-id1066999762?k=20&m=1066999762&s=612x612&w=0&h=BitPXyhBFZQHMfpC9ikX_DReVK6Rd28hH9pRoZW8YAs="
+          alt=""
+          className="fpImg"
+        />
+        <h4>Standard</h4>
+        <h6>Helsinki</h6>
+        <h4>Starting from € 150 </h4>
+        <div className="rating">
+          <button>8.5</button>
+          <h6>Very Good</h6>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://media.istockphoto.com/photos/3d-rendering-beautiful-luxury-bedroom-suite-in-hotel-with-tv-picture-id1066999762?k=20&m=1066999762&s=612x612&w=0&h=BitPXyhBFZQHMfpC9ikX_DReVK6Rd28hH9pRoZW8YAs="
+          alt=""
+          className="fpImg"
+        />
+        <h4>Standard</h4>
+        <h6>Helsinki</h6>
+        <h4>Starting from € 150 </h4>
+        <div className="rating">
+          <button>8.0</button>
+          <h6>Good</h6>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://media.istockphoto.com/photos/3d-rendering-beautiful-luxury-bedroom-suite-in-hotel-with-tv-picture-id1066999762?k=20&m=1066999762&s=612x612&w=0&h=BitPXyhBFZQHMfpC9ikX_DReVK6Rd28hH9pRoZW8YAs="
+          alt=""
+          className="fpImg"
+        />
+        <h4>Standard</h4>
+        <h6>Helsinki</h6>
+        <h4>Starting from € 150 </h4>
+        <div className="rating">
+          <button>9.3</button>
+          <h6>Wonderful</h6>
+        </div>
+      </div>
+      <div className="fpItem">
+        <img
+          src="https://media.istockphoto.com/photos/3d-rendering-beautiful-luxury-bedroom-suite-in-hotel-with-tv-picture-id1066999762?k=20&m=1066999762&s=612x612&w=0&h=BitPXyhBFZQHMfpC9ikX_DReVK6Rd28hH9pRoZW8YAs="
+          alt=""
+          className="fpImg"
+        />
+        <h4>Standard</h4>
+        <h6>Helsinki</h6>
+        <h4>Starting from € 150 </h4>
+        <div className="rating">
+          <button>8.3</button>
+          <h6>Very Good</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedPrice;
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import FeaturedCities from "../../components/featuredCities/FeaturedCities"
+import FeaturedPrice from "../../components/featuredPrice/FeaturedPrice"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import Property from "../../components/property/Property"
@@ -10,9 +11,12 @@ const Home = () => {
     <Navbar/>
     <Header/>
     <div className="homeContainer">
+      <h1 className="exploreFinland">Explore Finland</h1>
       <FeaturedCities/>
-      <h1 className="homeTitle">Browse by property types</h1>
+      <h1 className="homeTitle">Browse by property type</h1>
       <Property/>
+      <h1 className="homeTitle">Homes guests love</h1>
+      <FeaturedPrice/>
     </div>
     </>
   )
